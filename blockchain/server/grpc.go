a2ecdbb03deb9b382bd663f677799617942685e8
rafiul hasan chowdhury <rhasan346@gmail.com>
2022-09-18 10:53:12 +0600

    osmosis endpoint data fetched

--- blockchain/server/grpc.go
+++ blockchain/server/grpc.go
@@ -15,26 +15,26 @@ import (
 func (s *Server) Block(req *pb.GetLatestBlockRequest, stream pb.GetLatestBlockService_BlockServer) error{
 		println("Block stream called")
 
-		response, err := http.Get("https://rpc.osmosis.zone/abci_info?")
 
-		if err != nil {
-			fmt.Print(err.Error())
-			os.Exit(1)
-		}
-
-		responseData, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
 
 		for i:=0; i<10; i++{
+			response, err := http.Get("https://rpc.osmosis.zone/abci_info?")
 
+			if err != nil {
+				fmt.Print(err.Error())
+				os.Exit(1)
+			}
+
+			responseData, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
 			res := &pb.GetLatestBlockResponse{
 				BlockId: int32(i),
 				Block:  string(responseData),
 			}
 			stream.Send(res)
-			time.Sleep(3 * time.Second)
+			time.Sleep(6 * time.Second)
 		}
 		return nil
 	}
